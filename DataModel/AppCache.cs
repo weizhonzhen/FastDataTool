54bffc5f6101d09ed8f1fc6f9ba3d806cc500bb6
weizhonzhen <czdswzz@126.com>
2020-03-09 13:52:06 +0800

    Update AppCache.cs

--- DataModel/AppCache.cs
+++ DataModel/AppCache.cs
@@ -9,19 +9,10 @@ namespace DataModel
         private static string GetTableKey(DataLink link) { return string.Format("tableList_{0}_{1}_{2}", link.hostName, link.serverName, link.serverValue); }
         private static string GetViewKey(DataLink link) { return string.Format("viewList_{0}_{1}_{2}", link.hostName, link.serverName, link.serverValue); }
         private static string GetLinkKey(DataLink link) { return string.Format("LinkList_{0}_{1}_{2}", link.hostName, link.serverName, link.serverValue); }
-
-
+    
         //源数据库连接
         public static string GetTitle() { return title; }
         public static void SetTitle(string item) { title = item; }
-
-        //源数据库连接
-        public static DataLink GetSoureLink() { return DataCache.Get<DataLink>("soureLink"); }
-        public static void SetSoureLink(DataLink item) { DataCache.Set<DataLink>("soureLink", item); }
-
-        //目标数据库连接
-        public static DataLink GetTargetLink() { return DataCache.Get<DataLink>("targetLink"); }
-        public static void SetTargetLink(DataLink item) { DataCache.Set<DataLink>("targetLink", item); }
 
         //生成实体数据库连接
         public static DataLink GetBuildLink() { return DataCache.Get<DataLink>("buildLink"); }
@@ -37,20 +28,6 @@ namespace DataModel
         public static void SetViewList(List<BaseTable> item, DataLink link) { DataCache.Set<List<BaseTable>>(GetViewKey(link), item); }
         public static bool ExistsView(DataLink link) { return DataCache.Exists(GetViewKey(link)); }
 
-        //表列list
-        public static List<BaseColumn> GetColumnList() { return DataCache.Get<List<BaseColumn>>("columnList") ?? new List<BaseColumn>(); }
-        public static void SetColumnList(List<BaseColumn> item) { DataCache.Set<List<BaseColumn>>("columnList", item); }
-        public static bool ExistsColumnList() { return DataCache.Exists("columnList"); }
-
-        //视图列list
-        public static List<BaseColumn> GetColumnViewList() { return DataCache.Get<List<BaseColumn>>("columnViewList") ?? new List<BaseColumn>(); }
-        public static void SetColumnViewList(List<BaseColumn> item) { DataCache.Set<List<BaseColumn>>("columnViewList", item); }
-        public static bool ExistsColumnViewList() { return DataCache.Exists("columnViewList"); }
-
-        //自定义表
-        public static BaseTable GetDefineTable() { return DataCache.Get<BaseTable>("defineTable"); }
-        public static void SetDefineTable(BaseTable item) { DataCache.Set<BaseTable>("defineTable", item); }
-
         //表单元素
         public static List<FromItems> GetFromList() { return DataCache.Get<List<FromItems>>("fromItem")??new List<FromItems>(); }
         public static void SetFromList(List<FromItems> item) { DataCache.Set<List<FromItems>>("fromItem", item); }
@@ -63,26 +40,9 @@ namespace DataModel
         public static List<DefineSoureTable> GetDefineSoureTable() { return DataCache.Get<List<DefineSoureTable>>("defineSoureTable") ?? new List<DefineSoureTable>(); }
         public static void SetDefineSoureTable(List<DefineSoureTable> item) { DataCache.Set<List<DefineSoureTable>>("defineSoureTable", item); }
 
-        //目标表
-        public static List<BaseTable> GetTargetTable() { return DataCache.Get<List<BaseTable>>("targetTable") ?? new List<BaseTable>(); }
-        public static void SetTargetTable(List<BaseTable> item) { DataCache.Set<List<BaseTable>>("targetTable", item); }
-
-        //源表
-        public static List<SoureTable> GetSoureTable() { return DataCache.Get<List<SoureTable>>("soureTable") ?? new List<SoureTable>(); }
-        public static void SetSoureTable(List<SoureTable> item) { DataCache.Set<List<SoureTable>>("soureTable", item); }
-
-        //迁移结果
-        public static List<DataResult> GetResult() { return DataCache.Get<List<DataResult>>("result") ?? new List<DataResult>(); }
-        public static void SetResult(List<DataResult> item) { DataCache.Set<List<DataResult>>("result", item); }
-
-        //列对应
-        public static List<ColTargetSource> GetColList() { return DataCache.Get<List<ColTargetSource>>("colList") ?? new List<ColTargetSource>(); }
-        public static void SetColList(List<ColTargetSource> item) { DataCache.Set<List<ColTargetSource>>("colList", item); }
-
-        //表结构的列
-        public static List<BaseColumn> GetTableColumn(string key) { return DataCache.Get<List<BaseColumn>>(key) ?? new List<BaseColumn>(); }
+        //列
         public static void SetTableColumn(List<BaseColumn> item, string key) { DataCache.Set<List<BaseColumn>>(key, item); }
-        public static bool ExistsTableColumn(string key) { return DataCache.Exists(key); }
+        public static List<BaseColumn> GetTableColumn(string key) { return DataCache.Get<List<BaseColumn>>(key); }
 
         /// <summary>
         /// 是否联机
